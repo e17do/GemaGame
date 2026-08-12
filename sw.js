e596263779726bbcb2b46414af976c29933c1cf7
e17do <aljawi.fm@gmail.com>
2026-08-12 09:01:48 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,54 @@
-const CACHE_NAME = 'gema-cache-v2';
+const CACHE_NAME = "gema-cache-v3";
 
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-512.png',
-  './icon-192.png'
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./css/main.css",
+  "./js/storage.js",
+  "./js/state.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+
+      return fetch(event.request).then(response => {
+        if (!response || !response.ok) return response;
+
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      });
     })
   );
 });
